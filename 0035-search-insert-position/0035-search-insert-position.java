deeb9510ede3e0454a86e6a1695140273a998506
KTG5672 <38649590+KTG5672@users.noreply.github.com>
2025-10-22 10:15:49 +0900

    Time: 0 ms (100%), Space: 43.3 MB (19.85%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int length = nums.length;
+        int start = 0; int end = length - 1;
+        int pos = 0;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            pos = nums[mid] < target ? mid + 1 : mid;
+        }
+        return pos;
+    }
+}
